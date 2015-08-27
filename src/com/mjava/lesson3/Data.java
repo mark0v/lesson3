ee18e6e84f980c439b97d1f3fc00495b8dc6133e
unknown <a.markov@ws41hrk01.hrk.langate.internal>
2015-08-27 16:23:56 +0300

    Initial commit

---
+++ src/com/mjava/lesson3/Data.java
@@ -0,0 +1,47 @@
+package com.mjava.lesson3;
+
+public class Data {
+
+    private int q ;
+    private int w;
+    private int resDiv;
+    private int left;
+    
+    public int getQ() {
+        return q;
+    }
+
+    public int getW(){
+        return w;
+    }
+
+    public int getResDiv(){
+        return resDiv;
+    }
+
+    public int getLeft(){
+        return left;
+    }
+
+    public void setQ(int q){
+        this.q = q;
+    }
+
+    public void setW(int w){
+        this.w = w;
+    }
+
+    public void setResDiv(int resDiv){
+        this.resDiv = resDiv;
+    }
+
+    public void setLeft(int left){
+        this.left = left;
+    }
+    /*public String toString(){
+        return String.format("%d/%d = %d and %d left. ", q, w, resDiv, left);
+    }
+    */
+ }
+
+
---
+++ src/com/mjava/lesson3/Main.java
@@ -0,0 +1,53 @@
+package com.mjava.lesson3;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+        public static final int MAX_DIGITS = 10;
+        public static void main (String[] args){
+
+        System.out.println("Home Work 1: Task 1. ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter firs var: ");
+        int q = scanner.nextInt();
+        System.out.println("Enter second var: ");
+        int w = scanner.nextInt();
+        Data data = new Data();
+        data.setQ(q);
+        data.setW(w);
+        data.setResDiv(q / w);
+        data.setLeft(q % w);
+        //System.out.println(data);
+        System.out.println(q + "/" + w + " = " + data.getResDiv() + " and " + data.getLeft() + " left. " );
+        System.out.println("---------------------------------------------");
+        System.out.println("Home Work 1: Task 2 and 4. ");
+
+        System.out.println("Enter digits that will be sum. Maximum allowed value: 999999999 ");
+        int n = scanner.nextInt();
+
+        if (MAX_DIGITS<=999999999){
+            int [] a = new int[MAX_DIGITS];
+            int tmp = 0;
+            for (int i = MAX_DIGITS - 1; i >= 0; i-- ){
+                a[i] = n % 10;
+                n /= 10;
+                tmp +=a[i];
+            }
+            System.out.println(tmp);
+        }   else {
+            System.out.println("Your enter very big value. Try Again.");
+
+        }
+
+            System.out.println("---------------------------------------------");
+            System.out.println("Home Work 1: Task 3.%n Enter digits and the program round it.");
+            System.out.println("Enter digits and the program round it.");
+
+            float r = scanner.nextFloat();
+
+            System.out.println( "Roud: " + Math.round(r));
+    }
+}
+
