ad360d0f676857ef9b8b790947064363d64200b4
mark0v <amark0vv@gmail.com>
2015-09-11 08:11:35 +0300

    string Step 2

--- src/String/Complement.java
+++ src/String/Complement.java
@@ -5,47 +5,40 @@ public class Complement {
     public static void main(String[] args){
         StringBuffer input = new StringBuffer("ACGGAT");
         System.out.print(input + " Complement to: ");
+
         StringBuffer revers = input.reverse();
         StringBuffer compl;
 
-        char ch[] = new char[input.length()];
-        int tmp = 0;
+        char ch[] = new char[input.length()-1];
+        revers.getChars(0, revers.length()-1, ch, 0);
+        System.out.print(ch);
+
+        int start = input.length()-1;
+        int end = start;
+       // revers.replace(start, end, gen);
         for (int i = input.length()-1; i <= 0; i--){
-
-            int start = input.length()-1;
-            int end = start;
-
-            ch gen;
-            int genome = 4;
-
-            switch (regenome) {
-                case 1: if (revers.charAt(i) == 'A'){
-                    revers.replace(i, i, (char)T);
+            char tmp = ch[i];
+            switch (tmp) {
+                case 'A': revers.replace(i, i, "T");
+                    break;
+                case 'T': revers.replace(i, i, "A");
+                    break;
+                case 'C': revers.replace(i, i, "G");
+                    break;
+                case 'G': revers.replace(i, i, "C");
                     break;
                 }
-                case 2: if (revers.charAt(i) == 'T'){
-                    revers.replace(i, i, 'A');
-                    break;
-                }case 3: if (revers.charAt(i) == 'C'){
-                    revers.replace(i, i, 'G');
-                    break;
-                }case 4: if (revers.charAt(i) == 'G'){
-                    revers.replace(i, i, 'C');
-                    break;
-                }
-
-            }
-            revers.replace(start, end, gen);
+                ch[i]--;
+            System.out.println(ch[i]);
 
 
         }
 
 
-        System.out.print(input.reverse());
-
 
     }
-
-
-
 }
+
+
+
+
